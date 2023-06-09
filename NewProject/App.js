c668a82a29f5bf67ee4e4c958830f7494a79133c
sai-rishith <sairishith.chintala@archents.com>
2023-06-09 11:16:11 +0530

    final commit

--- NewProject/App.js
+++ NewProject/App.js
@@ -1,22 +1,3 @@
-// import { StyleSheet, Text, View } from 'react-native';
-// import LoginForm from './components/LoginForm';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//      <LoginForm/>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 
 import React from "react";
 import Navigation from "./components/Navigation";
