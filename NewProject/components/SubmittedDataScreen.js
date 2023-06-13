e787b34980a47e20cf7b155799217e970706852c
sai-rishith <sairishith.chintala@archents.com>
2023-06-13 15:16:15 +0530

    flatlist changes
    added some styles to the flatlist

--- NewProject/components/SubmittedDataScreen.js
+++ NewProject/components/SubmittedDataScreen.js
@@ -1,3 +1,4 @@
+// use the below code 
 
 import React from "react";
 import { View, Text, FlatList, StyleSheet, Button } from "react-native";
@@ -22,25 +23,131 @@ const SubmittedDataScreen = ({ navigation, route }) => {
           </View>
         )}
       />
-      <Button
-        title="Back to Login"
-        onPress={handleBackToLogin}
-        color="#008080"
-      />
+       <View style={styles.buttonContainer}>
+        <Button
+          title="Back to Login"
+          onPress={handleBackToLogin}
+          color="#008080"
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-    },
-    scrollContainer: {
-      flexGrow: 1,
-      justifyContent: "center",
-    },
-    
-  });
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 16,
+  },
+  buttonContainer: {
+    width: "100%",
+    marginTop: 16,
+    marginBottom:20,
+    justifyContent: "flex-start",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  itemContainer: {
+    backgroundColor: "#000",
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 8,
+  },
+  itemText: {
+    fontSize: 20,
+    color:'#fff',
+    marginBottom: 12,
+  },
+});
 
 export default SubmittedDataScreen;
+
+
+
+// import React, { useEffect, useState } from "react";
+// import { View, Text, FlatList, StyleSheet, Button } from "react-native";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// const SubmittedDataScreen = ({ navigation }) => {
+//   const [storedData, setStoredData] = useState([]);
+
+//   useEffect(() => {
+//     const retrieveData = async () => {
+//       try {
+//         const data = await AsyncStorage.getItem("submittedData");
+//         if (data) {
+//           setStoredData(JSON.parse(data));
+//         }
+//       } catch (error) {
+//         console.log("Error retrieving data:", error);
+//       }
+//     };
+
+//     retrieveData();
+//   }, []);
+
+//   const handleBackToLogin = () => {
+//     navigation.goBack();
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Submitted Data</Text>
+//       <FlatList
+//         data={storedData}
+//         keyExtractor={(item, index) => index.toString()}
+//         renderItem={({ item }) => (
+//           <View style={styles.itemContainer}>
+//             <Text style={styles.itemText}>Username: {item.username}</Text>
+//             <Text style={styles.itemText}>Password: {item.password}</Text>
+//           </View>
+//         )}
+//       />
+//       <View style={styles.buttonContainer}>
+//         <Button
+//           title="Back to Login"
+//           onPress={handleBackToLogin}
+//           color="#008080"
+//         />
+//       </View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     paddingHorizontal: 16,
+//   },
+//   buttonContainer: {
+//     width: "100%",
+//     marginTop: 16,
+//     marginBottom: 20,
+//     justifyContent: "flex-start",
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: "bold",
+//     marginBottom: 16,
+//   },
+//   itemContainer: {
+//     backgroundColor: "#000",
+//     borderRadius: 8,
+//     padding: 16,
+//     marginBottom: 8,
+//   },
+//   itemText: {
+//     fontSize: 20,
+//     color: "#fff",
+//     marginBottom: 12,
+//   },
+// });
+
+// export default SubmittedDataScreen;
