f2d47b93f2d7ccbc1844d4b9e97822a3891de20c
sai-rishith <sairishith.chintala@archents.com>
2023-06-20 16:14:58 +0530

    added AsyncStorage
    with features to save, edit, and delete submitted data

--- NewProject/components/LoginForm.js
+++ NewProject/components/LoginForm.js
@@ -402,6 +402,233 @@
 
 
 // below code is without scroll view and includes hide/show password feature
+// import React, { useState } from "react";
+// import {
+//   View,
+//   Text,
+//   TextInput,
+//   Dimensions,
+//   StyleSheet,
+//   Image,
+//   TouchableOpacity,
+// } from "react-native";
+// import { validateUserName, validatePassword } from "./Validation";
+
+// const LoginForm = ({ navigation }) => {
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [usernameError, setUsernameError] = useState("");
+//   const [passwordError, setPasswordError] = useState("");
+//   const [submittedData, setSubmittedData] = useState([]);
+//   const [passwordVisible, setPasswordVisible] = useState(false);
+//   const [rememberMe, setRememberMe] = useState(false);
+
+//   const handleLogin = () => {
+//     // Validate the username and password
+//     const usernameError = validateUserName(username);
+//     const passwordError = validatePassword(password);
+
+//     // Set the error messages in the state
+//     setUsernameError(usernameError);
+//     setPasswordError(passwordError);
+
+//     if (!usernameError && !passwordError) {
+//       const newData = {
+//         username,
+//         password,
+//       };
+
+//       setSubmittedData([...submittedData, newData]);
+//       setUsername("");
+//       setPassword("");
+//       navigation.navigate("SubmittedData", {
+//         submittedData: [...submittedData, newData],
+//       });
+//     }
+//   };
+
+//   const handleRegister = () => {
+//     navigation.navigate("Register");
+//   };
+
+//   const handleTogglePasswordVisibility = () => {
+//     setPasswordVisible(!passwordVisible);
+//   };
+
+//   const handleRememberMe = () => {
+//     setRememberMe(!rememberMe);
+//   };
+
+//   const windowWidth = Dimensions.get("window").width;
+//   const windowHeight = Dimensions.get("window").height;
+
+//   const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       justifyContent: "center",
+//       paddingHorizontal: windowWidth * 0.1,
+//       paddingTop: windowHeight * 0.05,
+//       paddingBottom: windowHeight * 0.05,
+//     },
+//     logoContainer: {
+//       alignItems: "center",
+//       marginBottom: windowHeight * 0.05,
+//     },
+//     logo: {
+//       width: windowWidth * 0.6,
+//       height: windowHeight * 0.2,
+//       resizeMode: "contain",
+//     },
+//     label: {
+//       fontSize: windowWidth * 0.04,
+//       fontWeight: "bold",
+//       marginBottom: windowHeight * 0.02,
+//     },
+//     inputContainer: {
+//       flexDirection: "row",
+//       alignItems: "center",
+//       borderWidth: 1,
+//       borderColor: "#ccc",
+//       borderRadius: 4,
+//       marginBottom: windowHeight * 0.025,
+//       paddingHorizontal: windowWidth * 0.02,
+//       height: windowHeight * 0.06,
+//     },
+//     input: {
+//       flex: 1,
+//       height: "100%",
+//     },
+//     eyeIcon: {
+//       padding: windowWidth * 0.02,
+//     },
+//     error: {
+//       color: "red",
+//       fontSize: windowWidth * 0.035,
+//       marginBottom: windowHeight * 0.015,
+//     },
+//     checkboxRow: {
+//       flexDirection: "row",
+//       alignItems: "center",
+//       marginTop: windowHeight * 0.02,
+//       marginBottom: windowHeight * 0.03,
+//     },
+//     rememberMeCheckbox: {
+//       width: windowWidth * 0.05,
+//       height: windowWidth * 0.05,
+//       marginRight: windowWidth * 0.02,
+//       borderWidth: 1,
+//       borderColor: "#ccc",
+//       borderRadius: 4,
+//       justifyContent: "center",
+//       alignItems: "center",
+//     },
+//     rememberMeText: {
+//       fontSize: windowWidth * 0.04,
+//     },
+//     forgotPasswordLink: {
+//       color: "#008080",
+//       textDecorationLine: "underline",
+//       marginLeft: windowWidth * 0.02,
+//     },
+//     loginButton: {
+//       backgroundColor: "#008080",
+//       paddingVertical: windowHeight * 0.015,
+//       borderRadius: 4,
+//       marginTop: windowHeight * 0.03,
+//     },
+//     loginButtonText: {
+//       color: "#fff",
+//       fontSize: windowWidth * 0.04,
+//       textAlign: "center",
+//     },
+//     registerContainer: {
+//       flexDirection: "row",
+//       marginTop: windowHeight * 0.02,
+//       marginBottom: windowHeight * 0.03,
+//     },
+//     registerText: {
+//       fontSize: windowWidth * 0.04,
+//     },
+//     registerLink: {
+//       color: "#008080",
+//       textDecorationLine: "underline",
+//       marginLeft: windowWidth * 0.01,
+//     },
+//   });
+
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.logoContainer}>
+//         <Image
+//           style={styles.logo}
+//           source={require("../assets/Docisn_logo.png")}
+//           resizeMode="contain"
+//         />
+//       </View>
+//       <Text style={styles.label}>Welcome to Docisn Provider App</Text>
+//       <Text style={styles.label}>Login to continue</Text>
+
+//       <Text style={styles.label}>Username:</Text>
+//       <View style={styles.inputContainer}>
+//         <TextInput
+//           style={styles.input}
+//           placeholder="Enter your username"
+//           value={username}
+//           onChangeText={setUsername}
+//         />
+//       </View>
+//       {usernameError !== "" && <Text style={styles.error}>{usernameError}</Text>}
+
+//       <Text style={styles.label}>Password:</Text>
+//       <View style={styles.inputContainer}>
+//         <TextInput
+//           style={styles.input}
+//           placeholder="Enter your password"
+//           secureTextEntry={!passwordVisible}
+//           value={password}
+//           onChangeText={setPassword}
+//         />
+//         <TouchableOpacity
+//           onPress={handleTogglePasswordVisibility}
+//           style={styles.eyeIcon}
+//         >
+//           <Text>{passwordVisible ? "Hide" : "Show"}</Text>
+//         </TouchableOpacity>
+//       </View>
+//       {passwordError !== "" && <Text style={styles.error}>{passwordError}</Text>}
+
+//       <View style={styles.checkboxRow}>
+//         <TouchableOpacity
+//           style={styles.rememberMeCheckbox}
+//           onPress={handleRememberMe}
+//         >
+//           {rememberMe && <Text>✓</Text>}
+//         </TouchableOpacity>
+//         <Text style={styles.rememberMeText}>Remember me</Text>
+//         <TouchableOpacity>
+//           <Text style={styles.forgotPasswordLink}>Forgot password?</Text>
+//         </TouchableOpacity>
+//       </View>
+
+//       <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+//         <Text style={styles.loginButtonText}>Login</Text>
+//       </TouchableOpacity>
+
+//       <View style={styles.registerContainer}>
+//         <Text style={styles.registerText}>Don't have an account?</Text>
+//         <TouchableOpacity onPress={handleRegister}>
+//           <Text style={styles.registerLink}>Register here</Text>
+//         </TouchableOpacity>
+//       </View>
+//     </View>
+//   );
+// };
+
+// export default LoginForm;
+
+
+
+//below code supports AsyncStorage
 import React, { useState } from "react";
 import {
   View,
@@ -412,6 +639,7 @@ import {
   Image,
   TouchableOpacity,
 } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { validateUserName, validatePassword } from "./Validation";
 
 const LoginForm = ({ navigation }) => {
@@ -423,7 +651,7 @@ const LoginForm = ({ navigation }) => {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
     // Validate the username and password
     const usernameError = validateUserName(username);
     const passwordError = validatePassword(password);
@@ -438,25 +666,25 @@ const LoginForm = ({ navigation }) => {
         password,
       };
 
-      setSubmittedData([...submittedData, newData]);
-      setUsername("");
-      setPassword("");
-      navigation.navigate("SubmittedData", {
-        submittedData: [...submittedData, newData],
-      });
+      try {
+        // Save submitted data to AsyncStorage
+        const storedData = await AsyncStorage.getItem("submittedData");
+        const existingData = storedData ? JSON.parse(storedData) : [];
+        const updatedData = [...existingData, newData];
+        await AsyncStorage.setItem("submittedData", JSON.stringify(updatedData));
+
+        setSubmittedData(updatedData);
+        setUsername("");
+        setPassword("");
+        navigation.navigate("SubmittedData", { submittedData: updatedData });
+      } catch (error) {
+        console.log("Error saving data:", error);
+      }
     }
   };
 
   const handleRegister = () => {
     navigation.navigate("Register");
-  };
-
-  const handleTogglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
-
-  const handleRememberMe = () => {
-    setRememberMe(!rememberMe);
   };
 
   const windowWidth = Dimensions.get("window").width;
@@ -589,7 +817,7 @@ const LoginForm = ({ navigation }) => {
           onChangeText={setPassword}
         />
         <TouchableOpacity
-          onPress={handleTogglePasswordVisibility}
+          onPress={() => setPasswordVisible(!passwordVisible)}
           style={styles.eyeIcon}
         >
           <Text>{passwordVisible ? "Hide" : "Show"}</Text>
@@ -600,7 +828,7 @@ const LoginForm = ({ navigation }) => {
       <View style={styles.checkboxRow}>
         <TouchableOpacity
           style={styles.rememberMeCheckbox}
-          onPress={handleRememberMe}
+          onPress={() => setRememberMe(!rememberMe)}
         >
           {rememberMe && <Text>✓</Text>}
         </TouchableOpacity>
@@ -625,7 +853,3 @@ const LoginForm = ({ navigation }) => {
 };
 
 export default LoginForm;
-
-
-
-
--- NewProject/components/SubmittedDataScreen.js
+++ NewProject/components/SubmittedDataScreen.js
@@ -1,12 +1,143 @@
-import React from "react";
-import { View, Text, FlatList, StyleSheet, Button } from "react-native";
+// import React from "react";
+// import { View, Text, FlatList, StyleSheet, Button } from "react-native";
 
-const SubmittedDataScreen = ({ navigation, route }) => {
-  const { submittedData } = route.params;
+// const SubmittedDataScreen = ({ navigation, route }) => {
+//   const { submittedData } = route.params;
 
-  function handleBackToLogin() {
-    navigation.goBack();
-  }
+//   function handleBackToLogin() {
+//     navigation.goBack();
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Submitted Data</Text>
+//       <FlatList
+//         data={submittedData}
+//         keyExtractor={(item, index) => index.toString()}
+//         renderItem={({ item }) => (
+//           <View style={styles.itemContainer}>
+//             <Text style={styles.itemText}>Username: {item.username}</Text>
+//             <Text style={styles.itemText}>Password: {item.password}</Text>
+//           </View>
+//         )}
+//       />
+//        <View style={styles.buttonContainer}>
+//         <Button
+//           title="Back to Login"
+//           onPress={handleBackToLogin}
+//           color="#008080"
+//         />
+//       </View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     paddingHorizontal: 16,
+//   },
+//   buttonContainer: {
+//     width: "100%",
+//     marginTop: 16,
+//     marginBottom:20,
+//     justifyContent: "flex-start",
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: "bold",
+//     marginBottom: 16,
+//   },
+//   itemContainer: {
+//     backgroundColor: "#000",
+//     borderRadius: 8,
+//     padding: 16,
+//     marginBottom: 8,
+//   },
+//   itemText: {
+//     fontSize: 20,
+//     color:'#fff',
+//     marginBottom: 12,
+//   },
+// });
+
+// export default SubmittedDataScreen;
+
+
+
+
+
+
+
+
+//below code with features to save, edit, and delete submitted data
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, StyleSheet, Button, TextInput } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const SubmittedDataScreen = ({ navigation }) => {
+  const [submittedData, setSubmittedData] = useState([]);
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [editedUsername, setEditedUsername] = useState("");
+  const [editedPassword, setEditedPassword] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const data = await AsyncStorage.getItem("submittedData");
+      if (data) {
+        setSubmittedData(JSON.parse(data));
+      }
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
+
+  const handleEditData = async () => {
+    try {
+      if (editingIndex !== null) {
+        const updatedData = [...submittedData];
+        const editedItem = { ...updatedData[editingIndex] };
+
+        editedItem.username = editedUsername;
+        editedItem.password = editedPassword;
+
+        updatedData[editingIndex] = editedItem;
+
+        await AsyncStorage.setItem("submittedData", JSON.stringify(updatedData));
+        setSubmittedData(updatedData);
+
+        setEditingIndex(null);
+        setEditedUsername("");
+        setEditedPassword("");
+      }
+    } catch (error) {
+      console.log("Error editing data:", error);
+    }
+  };
+
+  const handleDeleteData = async (index) => {
+    try {
+      const updatedData = [...submittedData];
+      updatedData.splice(index, 1);
+
+      await AsyncStorage.setItem("submittedData", JSON.stringify(updatedData));
+      setSubmittedData(updatedData);
+    } catch (error) {
+      console.log("Error deleting data:", error);
+    }
+  };
+
+  const handleEditButton = (index, item) => {
+    setEditingIndex(index);
+    setEditedUsername(item.username);
+    setEditedPassword(item.password);
+  };
 
   return (
     <View style={styles.container}>
@@ -14,20 +145,69 @@ const SubmittedDataScreen = ({ navigation, route }) => {
       <FlatList
         data={submittedData}
         keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
+        renderItem={({ item, index }) => (
           <View style={styles.itemContainer}>
             <Text style={styles.itemText}>Username: {item.username}</Text>
             <Text style={styles.itemText}>Password: {item.password}</Text>
+            <View style={styles.buttonContainer}>
+              <Button
+                title="Edit"
+                onPress={() => handleEditButton(index, item)}
+                color="#008080"
+              />
+              <Button
+                title="Delete"
+                onPress={() => handleDeleteData(index)}
+                color="#FF0000"
+              />
+            </View>
           </View>
         )}
       />
-       <View style={styles.buttonContainer}>
+      <View style={styles.buttonContainer}>
         <Button
           title="Back to Login"
-          onPress={handleBackToLogin}
+          onPress={() => navigation.goBack()}
           color="#008080"
         />
       </View>
+      {editingIndex !== null && (
+  <View style={styles.editContainer}>
+    <Text style={styles.editTitle}>Edit Data</Text>
+    <View style={styles.inputContainer}>
+      <Text style={styles.inputLabel}>Username:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter username"
+        value={editedUsername}
+        onChangeText={(text) => setEditedUsername(text)}
+      />
+    </View>
+    <View style={styles.inputContainer}>
+      <Text style={styles.inputLabel}>Password:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter password"
+        value={editedPassword}
+        onChangeText={(text) => setEditedPassword(text)}
+      />
+    </View>
+    <View style={styles.buttonContainer}>
+      <Button
+        title="Save"
+        onPress={handleEditData}
+        color="#008080"
+      />
+      <Button
+        title="Cancel"
+        onPress={() => setEditingIndex(null)}
+        color="#FF0000"
+      />
+    </View>
+  </View>
+)}
+
+
     </View>
   );
 };
@@ -40,15 +220,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
   },
   buttonContainer: {
-    width: "100%",
-    marginTop: 16,
-    marginBottom:20,
-    justifyContent: "flex-start",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 8,
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 16,
+  },
+  editTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 8,
   },
   itemContainer: {
     backgroundColor: "#000",
@@ -58,12 +242,53 @@ const styles = StyleSheet.create({
   },
   itemText: {
     fontSize: 20,
-    color:'#fff',
+    color: "#fff",
     marginBottom: 12,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    marginBottom: 8,
+    padding: 8,
+    width: "100%",
+  },
+  editContainer: {
+    backgroundColor: "#F0F0F0",
+    borderRadius: 8,
+    padding: 16,
+    marginVertical: 16,
+    width: "90%",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  inputContainer: {
+    marginBottom: 16,
+  },
+  inputLabel: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
   },
 });
 
 export default SubmittedDataScreen;
+
+
+
 
 
 
