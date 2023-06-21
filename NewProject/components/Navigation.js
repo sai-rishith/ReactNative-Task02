e02c3458f30faddda6c380981dd05285bda0f02d
sai-rishith <sairishith.chintala@archents.com>
2023-06-21 16:51:48 +0530

    Update Navigation.js

--- NewProject/components/Navigation.js
+++ NewProject/components/Navigation.js
@@ -4,6 +4,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import LoginForm from "../components/LoginForm";
 import RegistrationForm from "../components/RegistrationForm";
 import SubmittedDataScreen from "../components/SubmittedDataScreen";
+import ForgotPassword from "./ForgotPassword";
+import CreateNewPassword from "./CreateNewPassword";
 // import { AppRegistry } from "react-native";
 // import App from "../App";
 
@@ -17,8 +19,11 @@ const Navigation = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginForm} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
         <Stack.Screen name="Register" component={RegistrationForm} />
         <Stack.Screen name="SubmittedData" component={SubmittedDataScreen} />
+        <Stack.Screen name="CreatePassword" component={CreateNewPassword} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
